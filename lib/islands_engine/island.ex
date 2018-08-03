ece371701757366954a7246297fdbef276f272e7
Thomas Johnson <tjohnson1106@gmail.com>
2018-08-02 20:26:31 -0400

    created Island module

---
+++ lib/islands_engine/island.ex
@@ -0,0 +1,10 @@
+defmodule IslandsEngine.Island do
+  alias IslandsEngine.{Coordinate, Island}
+
+  @enforce_keys [:coordinates, :hit_coordinates]
+  defstruct [:coordinates, :hit_coordinates]
+
+  def new(), do:
+  %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+
+end
