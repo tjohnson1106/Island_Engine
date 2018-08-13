651397ced31b22bc165eafa085aacb452f0b4322
Thomas Johnson <tjohnson1106@gmail.com>
2018-08-13 16:49:38 -0400

    created gen server module game

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,3 +1,14 @@
-defmodule IslandEngine.Game do
+defmodule IslandsEngine.Game do
   use GenServer
+
+  def handle_info(:first, state) do
+    IO.puts "This message has been handled by handle_info/2, matching on :first"
+    {:noreply, state}
+  end
+
+  def handle_call(:demo_call, _from, state) do
+    {:reply, state, state}
+  end
 end
+
+
